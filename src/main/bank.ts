06fcab4fdb6c3857e59e42bb01115d625f7f4786
pragya18nsit <pragya18nsit@gmail.com>
2022-04-08 10:26:13 +0100

    update collaboratora

--- src/main/bank.ts
+++ src/main/bank.ts
@@ -1,6 +1,11 @@
 import { AccountService } from "./accountService";
 
 export class Bank implements AccountService {
+    printer : IStatementPrinter; // you can mock this
+    transatcionts : List<transactions>
+    constructor(IStatementPrinter printer) {
+        this.printer = printer;
+    }
     deposit(amount: number): void {
         throw new Error("Method not implemented.");
     }
@@ -8,6 +13,7 @@ export class Bank implements AccountService {
         throw new Error("Method not implemented.");
     }
     printStatement(): void {
+        this.printer.Print(transactions);
         throw new Error("Method not implemented.");
     }
 }
